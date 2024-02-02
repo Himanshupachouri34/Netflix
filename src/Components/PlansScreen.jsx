e38413783836e9bc7cd58392f9b18b23001a4efd
Himanshupachouri34 <himasnhupachouri34@gmail.com>
2024-02-02 22:24:08 +0530

    updated

--- src/Components/PlansScreen.jsx
+++ src/Components/PlansScreen.jsx
@@ -1,50 +1,77 @@
 import React from 'react'
 import '../Style/PlansScreen.css'
-import { loadStripe } from '@stripe/stripe-js';
 
 
 
 function PlansScreen() {
-  const itemName = "standard"
-  const itemPrice = 500
-  const quantity = 1
+    const amount = 500 * 100;
+    const currency = 'INR';
+    const receipt = 'qwerty'
 
 
-  const checkout = async () => {
-    const stripe = await loadStripe("pk_test_51OcyHsSGjiM5B4uog3RIVZJdvpd2KMgljvFFoZRoftfxZ0LqgWF9h2nUxANtLFIPcQOGn5Hm7fbQss8pUOqvwgz400lIzn6cjm")
+  const checkout = async (e) =>{
+    const response = await fetch("http://localhost:2000/order", {
+      method: "POST",
+      body: JSON.stringify({
+          amount,
+          currency,
+          receipt
+      }),
+      headers: {
+          "Content-Type" : "application/json"
+      }
+  })
+  const order = await response.json()
+  console.log(order);
 
-    try {
 
-      const res = await fetch("http://localhost:3000/checkout", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        mode: "cors",
-        body: JSON.stringify({
-          items: [
-            {
-              id: 1,
-              quantity: quantity,
-              price: itemPrice,
-              name: itemName
-            },
-          ]
-        })
+    var options = {
+      "key": "rzp_test_YjwKsc2YoXgoQF", // Enter the Key ID generated from the Dashboard
+      amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency,
+      "name": "Netflix", //your business name
+      "description": "Transaction",
+      "image": "https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png",
+      "order_id": order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      "handler": function (response){
+          alert(response.razorpay_payment_id);
+          alert(response.razorpay_order_id);
+          alert(response.razorpay_signature)
+      },
+      "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+          "name": "Himanshu", //your customer's name
+          "email": "himanshu.pachouri@SDE.com", 
+          "contact": "9000090000"  //Provide the customer's phone number for better conversion rates 
+      },
+      "notes": {
+          "address": "Razorpay Corporate Office"
+      },
+      "theme": {
+          "color": "#0ea40992"
+      }
+  };
+  var rzp1 = new window.Razorpay(options);
+  rzp1.on('payment.failed', function (response){
+          alert(response.error.code);
+          alert(response.error.description);
+          alert(response.error.source);
+          alert(response.error.step);
+          alert(response.error.reason);
+          alert(response.error.metadata.order_id);
+          alert(response.error.metadata.payment_id);
+  });
 
-      });
-      const data = await res.json();
-      window.location = data.url;
-    } catch (error) {
-      console.log(error);
-    }
+  rzp1.open();
+  e.preventDefault();
   }
+
+  
 
 
   return (
     <div className='PlansScreen'>
       <div className="plansScreen_info">
-        <h3>{itemName}</h3>
+        <h3>Standard</h3>
 
         <button onClick={checkout} >Subscribe</button>
       </div>
